4ed9f5648b2d5e655310a6adfef55a154dcf71a5
Akhilesh Yadav <113466625+Akhil32-web@users.noreply.github.com>
2026-02-18 14:19:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 //your JS code here. If required.
 function daysOfAYear(year) {
-  // Check leap year conditions
   if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
     return 366;
   } else {
     return 365;
   }
+}
+
+// Get input from user
+const year = Number(prompt("Enter year:"));
+
+// Show result
+alert(daysOfAYear(year));
