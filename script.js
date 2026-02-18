d794e8248e984ac4fa885c7a9b823ffcd4fb2f73
Akhilesh Yadav <113466625+Akhil32-web@users.noreply.github.com>
2026-02-18 13:49:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+  // Check leap year conditions
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366;
+  } else {
+    return 365;
+  }
